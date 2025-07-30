ca8836c1e470ce9c202409499804e20203b591f9
mariiiiiii <masum.s1343@gmail.com>
2025-07-30 09:50:26 -0700

    ArrayRandomFiller

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillArrayByRandom
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+        static void FillAndPrintArray(int[]arr , int index=0)
+        {
+            if (index == arr.Length)
+                return;
+            arr[index] = rnd.Next(1, 101);
+            Console.Write(arr[index] + " ");
+            FillAndPrintArray(arr, index + 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("size of array ? ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine(" array : ");
+            FillAndPrintArray(arr);
+
+        }
+    }
+}
